285952ee992038f5c43f458157ef526b9ec2cc6c
Marat Ibragimov <rigdillinger@yandex.ru>
2022-05-31 20:08:28 +0300

    Add Location model; Add validations and specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :validatable, :registerable
+
+  enum role: { viewer: 0, admin: 1 }
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,14 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  it { is_expected.to have_db_column(:email).of_type(:string) }
-  it { is_expected.to have_db_column(:encrypted_password).of_type(:string) }
-  it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
-  it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+  describe "schema" do
+    it { is_expected.to have_db_column(:email).of_type(:string) }
+    it { is_expected.to have_db_column(:encrypted_password).of_type(:string) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:email) }
+  end
 end
