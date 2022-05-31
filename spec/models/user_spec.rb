7182cb86716f50736de9d4aff693842c8eec5519
Marat Ibragimov <rigdillinger@yandex.ru>
2022-05-31 18:49:42 +0300

    Add User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  it { is_expected.to have_db_column(:email).of_type(:string) }
+  it { is_expected.to have_db_column(:encrypted_password).of_type(:string) }
+  it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+  it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+end
